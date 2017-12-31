ab4b0c8fb4db3ccb2e7ecfb569f80439b9421f84
piacere-ex <masq_wise@hotmail.com>
2017-12-31 17:00:18 +0900

    New

---
+++ README.md
@@ -0,0 +1,25 @@
+# Quantex
+
+[Quantex](https://hex.pm/packages/quantex) is a quantum computer system environment and libraries in Elixir. Here is an example:
+
+```elixir
+iex> 
+
+```
+
+See the [online documentation](https://hexdocs.pm/quantex).
+
+## Installation
+
+Add to your ```mix.exs``` file:
+
+```elixir
+def deps do
+  [
+    { :quantex, "~> 0.0.1" }
+  ]
+end
+```
+
+## License
+This project is licensed under the terms of the Apache 2.0 license, see LICENSE.
---
+++ mix.exs
@@ -0,0 +1,36 @@
+defmodule Quantex.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :quantex,
+      version: "0.0.1",
+      elixir: "~> 1.5",
+      description: "Quantum computer system environment and libraries in Elixir", 
+      package: 
+      [
+        maintainers: [ "piacere" ], 
+        licenses:    [ "Apache 2.0" ], 
+        links:       %{ "GitHub" => "https://github.com/piacere-ex/quantex" }, 
+      ],
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      { :ex_doc,         "~> 0.16",  only: :dev, runtime: false }, 
+      { :power_assert,   "~> 0.1.1", only: :test }, 
+      { :mix_test_watch, "~> 0.5",   only: :dev, runtime: false }, 
+    ]
+  end
+end
